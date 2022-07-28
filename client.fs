b7a24065ff29b1452bc347ecf9baeb5002e3b1e9
Christiaan Brinkerink <brinkie@gmail.com>
2022-07-28 11:50:36 +0200

    Implemented sky-dome direction tracking

--- client.fs
+++ client.fs
@@ -19,56 +19,74 @@ uniform sampler2D texture1;
 
 // Microphone positions
 // Offset from mic1: 0.073, 0.065
-vec2 mic1pos = vec2(-0.073, -0.065);
-vec2 mic2pos = vec2(0.073, -0.065);
-vec2 mic3pos = vec2(-0.038, 0.065);
-vec2 mic4pos = vec2(0.04, 0.065);
+vec3 mic1pos = vec3(-0.073, -0.065, 0.);
+vec3 mic2pos = vec3(0.073, -0.065, 0.);
+vec3 mic3pos = vec3(-0.038, 0.065, 0.);
+vec3 mic4pos = vec3(0.04, 0.065, 0.);
 
-float pixelscale = 2000.; // Pixels per meter
-// NOTE: On retina screens this is apparently different by a factor of 2
+float pixelscale = 600.; // Pixels per meter
+// NOTE: On retina screens this is apparently different by a factor of 2:
+// my laptop has an apparent window size that is twice the numbers here.
+// To correctly calculate the middle of the window, I have to multiply the
+// coords for the middle of the window (400,300 in this example) by 2.
 float windowWidth = 800.;
 float windowHeight = 600.;
 float soundspeed = 343.;
 float samplerate = 46875.;
 
+float domeradius = 1.; // Use 1 m for sky dome radius for now
+
 void main()
 {
-    FragColor = texture(texture1, TexCoord);
-    vec2 r_mic1 = (gl_FragCoord.xy - vec2(windowWidth, windowHeight)) / pixelscale - mic1pos;
-    vec2 r_mic2 = (gl_FragCoord.xy - vec2(windowWidth, windowHeight)) / pixelscale - mic2pos;
-    vec2 r_mic3 = (gl_FragCoord.xy - vec2(windowWidth, windowHeight)) / pixelscale - mic3pos;
-    vec2 r_mic4 = (gl_FragCoord.xy - vec2(windowWidth, windowHeight)) / pixelscale - mic4pos;
+    // Test code to display lag texture directly
+    //FragColor = texture(texture1, TexCoord);
 
-    // Baseline 1-2
-    float d_12 = length(r_mic2) - length(r_mic1);
-    int lag_12 = int(round(samplerate * d_12 / soundspeed));
-    // Baseline 2-3
-    float d_23 = length(r_mic3) - length(r_mic2);
-    int lag_23 = int(round(samplerate * d_23 / soundspeed));
-    // Baseline 3-1
-    float d_31 = length(r_mic1) - length(r_mic3);
-    int lag_31 = int(round(samplerate * d_31 / soundspeed));
-    // Baseline 1-4
-    float d_14 = length(r_mic4) - length(r_mic1);
-    int lag_14 = int(round(samplerate * d_14 / soundspeed));
-    // Baseline 2-4
-    float d_24 = length(r_mic4) - length(r_mic2);
-    int lag_24 = int(round(samplerate * d_24 / soundspeed));
-    // Baseline 3-4
-    float d_34 = length(r_mic4) - length(r_mic3);
-    int lag_34 = int(round(samplerate * d_34 / soundspeed));
+    // Use a projected sky dome. We define some effective radius (far-field), and calculate each pixel's
+    // effective x,y,z coords using that. We then calculate the distances to each mic from that dome position.
 
-    vec4 brightness = 
-                       (texture(texture1, vec2(float(lag_12 + 32) / 64., 3./16.)).r * vec4(1., 0., 0., 0.4) +
-                        texture(texture1, vec2(float(lag_23 + 32) / 64., 5./16.)).r * vec4(0., 1., 0., 0.4) +
-                        texture(texture1, vec2(float(lag_31 + 32) / 64., 7./16.)).r * vec4(0., 0., 1., 0.4) +
-                        texture(texture1, vec2(float(lag_14 + 32) / 64., 9./16.)).r * vec4(0.7, 0.7, 0., 0.4) +
-                        texture(texture1, vec2(float(lag_24 + 32) / 64., 11./16.)).r * vec4(0.7, 0., 0.7, 0.4) +
-                        texture(texture1, vec2(float(lag_34 + 32) / 64., 13./16.)).r * vec4(0., 0.7, 0.7, 0.4))/6.;
-    FragColor = brightness;
+    vec3 pixelpos = vec3((gl_FragCoord.xy - vec2(windowWidth, windowHeight)) / pixelscale, 0.);
 
-    if (length(r_mic1) < 0.01 || length(r_mic2) < 0.01 || length(r_mic3) < 0.01 || length(r_mic4) < 0.01) {
-        FragColor = vec4(0., 0., 1., 1.);
+    if (length(pixelpos) <= domeradius) {
+      pixelpos.z = sqrt(domeradius * domeradius - pixelpos.x * pixelpos.x - pixelpos.y * pixelpos.y);
+
+      vec3 r_mic1 = pixelpos - mic1pos;
+      vec3 r_mic2 = pixelpos - mic2pos;
+      vec3 r_mic3 = pixelpos - mic3pos;
+      vec3 r_mic4 = pixelpos - mic4pos;
+
+      // Baseline 1-2
+      float d_12 = length(r_mic2) - length(r_mic1);
+      int lag_12 = int(round(samplerate * d_12 / soundspeed));
+      // Baseline 2-3
+      float d_23 = length(r_mic3) - length(r_mic2);
+      int lag_23 = int(round(samplerate * d_23 / soundspeed));
+      // Baseline 3-1
+      float d_31 = length(r_mic1) - length(r_mic3);
+      int lag_31 = int(round(samplerate * d_31 / soundspeed));
+      // Baseline 1-4
+      float d_14 = length(r_mic4) - length(r_mic1);
+      int lag_14 = int(round(samplerate * d_14 / soundspeed));
+      // Baseline 2-4
+      float d_24 = length(r_mic4) - length(r_mic2);
+      int lag_24 = int(round(samplerate * d_24 / soundspeed));
+      // Baseline 3-4
+      float d_34 = length(r_mic4) - length(r_mic3);
+      int lag_34 = int(round(samplerate * d_34 / soundspeed));
+
+      vec4 brightness = 
+                         (texture(texture1, vec2(float(lag_12 + 32) / 64., 3./16.)).r * vec4(1., 0., 0., 0.4) +
+                          texture(texture1, vec2(float(lag_23 + 32) / 64., 5./16.)).r * vec4(0., 1., 0., 0.4) +
+                          texture(texture1, vec2(float(lag_31 + 32) / 64., 7./16.)).r * vec4(0., 0., 1., 0.4) +
+                          texture(texture1, vec2(float(lag_14 + 32) / 64., 9./16.)).r * vec4(0.7, 0.7, 0., 0.4) +
+                          texture(texture1, vec2(float(lag_24 + 32) / 64., 11./16.)).r * vec4(0.7, 0., 0.7, 0.4) +
+                          texture(texture1, vec2(float(lag_34 + 32) / 64., 13./16.)).r * vec4(0., 0.7, 0.7, 0.4))/2.4;
+      FragColor = brightness;
+
+      if (length(r_mic1) < 0.005 || length(r_mic2) < 0.005 || length(r_mic3) < 0.005 || length(r_mic4) < 0.005) {
+          FragColor = vec4(0., 0., 1., 1.);
+      }
+    } else {
+      FragColor = vec4(0., 0., 0., 1.);
     }
 
     if (gl_FragCoord.y < 60. && gl_FragCoord.y >= 50.) {
